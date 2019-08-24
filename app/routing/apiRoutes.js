3bded52a4cad730c60f781840febdff36eda7baf
Lorena Ontiveros <lorena0328o@gmail.com>
2019-08-23 20:56:17 -0500

    routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,21 @@
 //get a route with the url /api/friends. this will be used to display a json of all possible friends
 // post routes /api/friends. this will be used to handle incoming survey results. this route will also be used to hand compatibility logic.
+// We are linking our routes to a series of "data" sources.
+
+// express integration
+var express = require('express');
+var app = express();
+// path integration
+var path = require('path');
+// port location
+const port = 3030;
+
+// get request to return friends json object to client
+app.get('/api/friends', function(req, res){
+    return res.json(/* VARIABLE NAME/REFERENCE TO FRIENDS DATA */)
+});
+
+// post to handle incoming data
+app.post('/api/friends', function(req, res){
+    /* CODE TO HANDLE INCOMING DATA AND POST TO DATABASE + HANDLE COMPATABILITY LOGIC */
+})
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,19 @@
 // get a route to /survey which would display the survey page
 // default, catch-all route that leads to home.html which displays the homepage
+// express integration
+var express = require('express');
+var app = express();
+// path integration
+var path = require('path');
+// port location
+const port = 3030;
+
+// get request to send home.html by default
+app.get('/', function(req, res){
+
+});
+
+// get request to send survey.html to client
+app.get('/survey', function(req, res){
+
+});
