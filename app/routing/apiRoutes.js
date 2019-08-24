928d6e298c24b611d231e7bb5dc8697ba845b3b8
Lorena Ontiveros <lorena0328o@gmail.com>
2019-08-24 00:18:37 -0500

    honestly just stuff

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,3 +19,34 @@ app.get('/api/friends', function(req, res){
 app.post('/api/friends', function(req, res){
     /* CODE TO HANDLE INCOMING DATA AND POST TO DATABASE + HANDLE COMPATABILITY LOGIC */
 })
+//##############################################################################################################
+function findMatch(newbie) {
+    var friendDataRefresh = friendsData;
+
+    // loop through each friend minus the one (the user whose input was just added)
+    for (var i = 0; i < friendDataRefresh.length; i++) {
+        friendDataRefresh[i].difference = [];
+        friendDataRefresh[i].totalDifference;
+
+        for (var j = 0; j < friendDataRefresh[i].scores.length; j++) {
+            // loops through scores; pushes to array
+            friendDataRefresh[i].difference.push(Math.abs(newbie.scores[j] - friendDataRefresh[i].scores[j]));
+            friendDataRefresh[i].totalDifference = friendDataRefresh[i].difference.reduce((a, b) => a + b, 0);
+        }
+    }
+    var totalDiffArr = [];
+    for (var i = 0; i < friendDataRefresh.length; i++) {
+        totalDiffArr.push(friendDataRefresh[i].totalDifference);
+        var index = 0;
+        var value = totalDiffArr[0];
+        for (var j = 1; j < friendDataRefresh.length; j++) {
+            if (totalDiffArr[j] < value) {
+                value = totalDiffArr[j];
+                index = j;
+            }
+        }
+
+    }
+    return friendDataRefresh[index];
+
+}
